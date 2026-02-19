5e8bbec03ffb2a0eadcd5278ca7fa8fb9652daf7
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2026-02-18 18:18:10 -0600

    python-rest-client-server

--- APPLICATION/python-rest-client-server/client/src/web/ApplicationController.py
+++ APPLICATION/python-rest-client-server/client/src/web/ApplicationController.py
@@ -13,23 +13,24 @@ class ApplicationController:
     @staticmethod 
     def getEmployeeService(self) -> EmployeeService:
         return self.employeeService
-        
+
+    @staticmethod   
     @app.get("/api/employees/{employeeId}")   
     def getEmployeeById( employeeId: int, service = Depends(getEmployeeService)) -> Employee:
         return service.getEmployeeById(employeeId)
-    
+    @staticmethod
     @app.get("/api/employees")
     def getAllEmployees(service = Depends(getEmployeeService)) -> list[Employee]:
         return service.getAllEmployees()
-    
+    @staticmethod
     @app.post("/api/employees")
     def addEmployee( employee: Employee, service = Depends(getEmployeeService)) -> str:
         return service.addEmployee(employee)
-    
+    @staticmethod
     @app.put("/api/employees/{employeeId}")
     def updateEmployee( employeeId: int, employee: Employee, service = Depends(getEmployeeService)) -> str:
         return service.updateEmployee(employeeId, employee)
-    
+    @staticmethod
     @app.delete("/api/employees/{employeeId}")
     def deleteEmployee( employeeId: int, service = Depends(getEmployeeService)) -> str:
         return service.deleteEmployee(employeeId)
